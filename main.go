219ece914f2340eb37e24be5c44aadfcbffe09c4
Jacob Williams <jacobaw@gmail.com>
2017-11-11 00:05:19 -0600

    ex 4.4

--- main.go
+++ main.go
@@ -56,6 +56,22 @@ func ex3_10() {
 	fmt.Printf("Successfully demonstrated complementation property? %v\n", bytes.Equal(ciphertext, ciphertext3))
 }
 
+func ex4_4() {
+	key, _ := hex.DecodeString("8000000000000000000000000000000000000000000000000000000000000001")
+	cipher, _ := aes.NewCipher(key)
+	ciphertext, _ := hex.DecodeString("87F348FF79B811AF3857D6718E5F0F917C3D26F77377635A5E43E9B5CC5D05926E26FFC5220DC7D405F1708670E6E017")
+	plaintext := make([]byte, 32, 32)
+	for i := 16; i < 48; i += 16 {
+		prevcipherblock := ciphertext[i-16 : i]
+		curcipherblock := ciphertext[i : i+16]
+		cipher.Decrypt(plaintext[i-16:i], curcipherblock)
+		for j := 0; j < 16; j++ {
+			plaintext[i-16+j] ^= prevcipherblock[j]
+		}
+	}
+	fmt.Println(string(plaintext))
+}
+
 func main() {
 	ex := os.Args[1]
 	switch ex {
@@ -65,5 +81,7 @@ func main() {
 		ex3_9()
 	case "3.10":
 		ex3_10()
+	case "4.4":
+		ex4_4()
 	}
 }
